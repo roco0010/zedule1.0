d021005bccfa92ab97a497f701159a4d9561112d
Metafe <98275632+roco0010@users.noreply.github.com>
2026-02-15 20:37:42 -0500

    Replace Firebase secrets with environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -10,8 +10,10 @@ const firebaseConfig = {
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
+
+export default firebaseConfig;
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
